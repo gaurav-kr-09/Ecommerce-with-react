0f4730ea0b99b594192e4a474b4c7bd5b2e42248
Gaurav Kumar <gauravkrthakur09@gmail.com>
2025-07-10 00:46:31 +0530

    Add server proxy config for /api and /images

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  //niche jo kar re hai usse ye hoga ki hamko bar bar pura web address nahi type karna padega. Agar Ye kar denge to
+  //jyohi koi chiz /api se start hoga to ye automatic is link ko call kar dega.
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000'
+      },
+      '/images': {
+        target: 'http://localhost:3000'
+      }
+    }
+  }
 })
