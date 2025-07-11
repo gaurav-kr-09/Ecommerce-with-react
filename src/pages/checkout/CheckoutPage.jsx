9b5bcd254c72f7c3535aec1aabc0f9c36b8b4ccd
Gaurav Kumar <gauravkrthakur09@gmail.com>
2025-07-11 23:04:55 +0530

    seperate orders grid  and checkout header in a different component

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -26,7 +26,7 @@ export function CheckoutPage({ cart }) {
     <>
       <title>Checkout</title>
       <link rel="shortcut icon" href="cart-favicon.png" type="image/x-icon" />
-      <CheckoutHeader />
+      <CheckoutHeader cart={cart} />
 
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
