89a8e545220cc19206b896c29fea72e94958241a
Gaurav Kumar <gauravkrthakur09@gmail.com>
2025-07-13 13:04:32 +0530

    Made update button in checkout page working

--- src/pages/checkout/CartItemDetails.jsx
+++ src/pages/checkout/CartItemDetails.jsx
@@ -1,11 +1,45 @@
 import axios from "axios";
 import { formatMoney } from "../../utils/money";
+import { useState } from "react";
 
 export function CartItemDetails({ cartItem, loadCart }) {
+  const [isquanityUpdated, setIsQuantityUpdated] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
   };
+
+  const updateQuantity = async () => {
+    if (isquanityUpdated) {
+      if (quantity <= 0) {
+        deleteCartItem();
+      } else {
+        await axios.put(`/api/cart-items/${cartItem.productId}`,{
+          quantity: Number(quantity),
+        })
+      };
+      await loadCart();
+      setIsQuantityUpdated(false);
+    } else {
+      setIsQuantityUpdated(true);
+    }
+  }
+
+  const updateQuantityInput = (event) => {
+    setQuantity(event.target.value);
+  }
+
+  function handleKeyboard(event) {
+    const pressedKey = event.key;
+    if(pressedKey === 'Enter'){
+      updateQuantity();
+    }else if (pressedKey === 'Escape'){
+      setQuantity(cartItem.quantity);
+      setIsQuantityUpdated(false);
+    }
+  }
 
   return (
     <>
@@ -21,10 +55,19 @@ export function CartItemDetails({ cartItem, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity:
+            {isquanityUpdated ? 
+            <input type="number" min={0} 
+            className="quantity-input"
+            value={quantity} 
+            onChange={updateQuantityInput} 
+            onKeyDown={handleKeyboard} /> :
+              <span className="quantity-label">
+                {cartItem.quantity}</span>
+            }
           </span>
-          <span className="update-quantity-link link-primary">
-            Update
+          <span className="update-quantity-link link-primary" onClick={updateQuantity}>
+            {isquanityUpdated ? 'Save' : 'Update'}
           </span>
           <span className="delete-quantity-link link-primary"
             onClick={deleteCartItem}>
