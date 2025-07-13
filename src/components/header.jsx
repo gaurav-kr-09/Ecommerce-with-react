4db6370eb3480ba19134dc8b7acd25580468abcc
Gaurav Kumar <gauravkrthakur09@gmail.com>
2025-07-13 14:53:49 +0530

    made search interactive

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,27 @@
-import { NavLink } from 'react-router';
+import { NavLink, useNavigate, useSearchParams } from 'react-router';
 import cartIcon from '../assets/images/icons/cart-icon.png';
 import SearchIcon from '../assets/images/icons/search-icon.png';
 import LogoWhite from '../assets/images/logo-white.png';
 import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
+import { useState } from 'react';
 
 import './Header.css';
 
 export function Header({ cart }) {
+  const navigate = useNavigate();
+  
+  const [searchParams] = useSearchParams();
+  const searchText = searchParams.get('seach');
+  const [search, setSearch] = useState(searchText || '');
+
+  const updateSearchInput = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchProducts = () => {
+    navigate(`/?search=${search}`);
+  };
+
   let totalQuantity = 0;
 
   cart.forEach(cartItem => {
@@ -26,9 +41,9 @@ export function Header({ cart }) {
         </div>
 
         <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
+          <input className="search-bar" type="text" placeholder="Search" value={search} onChange={updateSearchInput} />
 
-          <button className="search-button">
+          <button className="search-button" onClick={searchProducts}>
             <img className="search-icon" src={SearchIcon} />
           </button>
         </div>
