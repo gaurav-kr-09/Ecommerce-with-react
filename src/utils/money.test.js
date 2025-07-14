1d9b2c84344a26cd86659c7156de45fe55887d4b
Gaurav Kumar <gauravkrthakur09@gmail.com>
2025-07-15 00:29:33 +0530

    Add test in formatMoney for 0 and -ve money

--- src/utils/money.test.js
+++ src/utils/money.test.js
@@ -9,5 +9,14 @@ describe('formatMoney', () => {
   it('displays 2 decimals', () => {
     expect(formatMoney(1090)).toBe('$10.90');
     expect(formatMoney(100)).toBe('$1.00')
+  });
+
+  it('Formats 0 cents as $0.00', () => {
+    expect(formatMoney(0)).toBe('$0.00');
+  });
+
+  it('Formats -ve money', () => {
+    expect(formatMoney(-999)).toBe('-$9.99');
+    expect(formatMoney(-100)).toBe('-$1.00');
   })
 });
