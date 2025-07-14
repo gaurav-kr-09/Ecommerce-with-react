0d23aac12d91c623cd3e4bde4696d0201356e0bc
Gaurav Kumar <gauravkrthakur09@gmail.com>
2025-07-15 01:51:05 +0530

    Add automated test for add to cart button in homepage for different products

--- src/pages/Home/HomePage.test.jsx
+++ src/pages/Home/HomePage.test.jsx
@@ -3,11 +3,13 @@ import { render, screen, within } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import { HomePage } from './HomePage';
 import axios from 'axios';
+import userEvent from '@testing-library/user-event';
 
 vi.mock('axios');
 
 describe('HomePage component', () => {
   let loadCart;
+  let user;
 
   beforeEach(() => {
     loadCart = vi.fn();
@@ -40,6 +42,8 @@ describe('HomePage component', () => {
         }
       }
     });
+
+    user = userEvent.setup();
   });
 
   it('displays the products correctly', async () => {
@@ -65,4 +69,32 @@ describe('HomePage component', () => {
 
 
   });
+
+  it('Add to cart button works', async () => {
+    render(
+      <MemoryRouter>
+        <HomePage cart={[]} loadCart={loadCart} />
+      </MemoryRouter>
+    );
+
+    const productContainers = await screen.findAllByTestId('product-container')
+
+    const addToCartButton1 =  within(productContainers[0]).getByTestId('add-to-cart-button');
+    await user.click(addToCartButton1);
+
+    const addToCartButton2 =  within(productContainers[1]).getByTestId('add-to-cart-button');
+    await user.click(addToCartButton2);
+
+    expect(axios.post).toHaveBeenNthCalledWith(1, '/api/cart-items', {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1
+    });
+
+    expect(axios.post).toHaveBeenNthCalledWith(2, '/api/cart-items', {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1
+    });
+    expect(loadCart).toHaveBeenCalledTimes(2);
+
+  })
 });
