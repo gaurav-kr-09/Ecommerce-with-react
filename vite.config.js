ae6518fdc58035a908310b038d3bfa9ad485624d
Gaurav Kumar <gauravkrthakur09@gmail.com>
2025-07-23 22:11:32 +0530

    downloaded react compiler

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [['babel-plugin-react-compiler', { target: '19' }]],
+    },
+  })],
 
   //niche jo kar re hai usse ye hoga ki hamko bar bar pura web address nahi type karna padega. Agar Ye kar denge to
   //jyohi koi chiz /api se start hoga to ye automatic is link ko call kar dega.
